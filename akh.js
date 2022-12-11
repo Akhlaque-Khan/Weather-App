02f0d976e3212905a358f116aa5f40bccdb28248
Akhlaque-Khan <khanakhlakh123@gmail.com>
2022-12-11 22:04:07 +0530

    initial

--- akh.js
+++ akh.js
@@ -24,17 +24,30 @@ function showWeatherReport(weather){
     console.log(weather);
     let city = document.getElementById('city');
     city.innerText=`${weather.name},${weather.sys.country}`;
+
     let temperature = document.getElementById('temp');
     temperature.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
+
     let min=document.getElementById('min');
     min.innerHTML=`${Math.floor(weather.main.temp_min)}&deg;C (Min) / ${Math.ceil(weather.main.temp_max)}&deg;C (Max)`;
-    let weatherType=document.getElementById('weather');
-    weatherType.innerText=`${weather.weather[0].main}`;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerHTML = `${weather.weather[0].main}`;
+   
+    
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerHTML=dateManage(todayDate);
+
 
 }
-
-
-
-
-
 //manage date
+function dateManage(dateArg){
+    let days = ["Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday"];
+    let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+    return `${date} ${month} (${day}), ${year}`;
+}
